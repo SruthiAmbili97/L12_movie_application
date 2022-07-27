607a1646bfdd466a52a8ab0572b6fc4375ddc439
SruthiAmbili97 <21006564@myrp.edu.sg>
2022-07-27 15:56:09 +0800

    showmovie

--- app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/ShowMovies.java
+++ app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/ShowMovies.java
@@ -3,14 +3,24 @@ package sg.edu.rp.c346.id21023701.movieapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShowMovies extends AppCompatActivity {
 
-
+   ListView lvShow;
+   ArrayList<movie>alMovieList;
+   CustomerAdapter caMovie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_movies);
 
+        lvShow =findViewById(R.id.lvShow);
+        alMovieList = new ArrayList<movie>();
+
+        caMovie = new CustomerAdapter(this.R.layout.row,alMovieList);
+        lvShow.setAdapter(caMovie);
     }
 }
