83ed4afc0b43b3368cd49b16d07deb907e66a922
SruthiAmbili97 <21006564@myrp.edu.sg>
2022-07-27 16:11:19 +0800

    string,row,ca,show

--- app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/CustomerAdapter.java
+++ app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/CustomerAdapter.java
@@ -1,5 +1,11 @@
 package sg.edu.rp.c346.id21023701.movieapplication;
 
-public class CustomerAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class CustomerAdapter  {
+
 
 }
