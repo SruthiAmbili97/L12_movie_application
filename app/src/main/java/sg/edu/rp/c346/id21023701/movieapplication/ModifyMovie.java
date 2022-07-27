6aa50d10f7fe3770c508bf65d5faf00e554296e1
SruthiAmbili97 <21006564@myrp.edu.sg>
2022-07-27 15:09:38 +0800

    second and third activity

--- app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/ModifyMovie.java
+++ app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/ModifyMovie.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id21023701.movieapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,13 +13,23 @@ public class ModifyMovie extends AppCompatActivity {
     TextView tvTitle,tvId,tvGenere,tvYear,TvRating,tvSpinner;
     EditText etTitle,etId,etGenere,etYear,etRating;
     Spinner spinRating;
-    Button btnDelete,btnClose,btnUpdate;
+    Button btnDelete,btnCancel,btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_movie);
 
-        btnUpdate
+        btnUpdate =findViewById(R.id.btnUpdate);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnDelete = findViewById(R.id.btnDelete);
+        etId = findViewById(R.id.etID);
+        etTitle = findViewById(R.id.etTitile2);
+        etGenere = findViewById(R.id.etGenere2);
+        etYear = findViewById(R.id.etYear2);
+     spinRating = findViewById(R.id.spinRating2);
+
     }
+
+
 }
--- app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/ShowMovies.java
+++ app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/ShowMovies.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class ShowMovies extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
