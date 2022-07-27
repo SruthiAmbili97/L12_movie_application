c9700c21b2b598d2bbed85e681a1bb14ab342f4c
SruthiAmbili97 <21006564@myrp.edu.sg>
2022-07-27 14:30:44 +0800

    thirdSatement

--- app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/ModifyMovie.java
+++ app/src/main/java/sg/edu/rp/c346/id21023701/movieapplication/ModifyMovie.java
@@ -3,12 +3,22 @@ package sg.edu.rp.c346.id21023701.movieapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ModifyMovie extends AppCompatActivity {
+    TextView tvTitle,tvId,tvGenere,tvYear,TvRating,tvSpinner;
+    EditText etTitle,etId,etGenere,etYear,etRating;
+    Spinner spinRating;
+    Button btnDelete,btnClose,btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_movie);
+
+        btnUpdate
     }
 }
